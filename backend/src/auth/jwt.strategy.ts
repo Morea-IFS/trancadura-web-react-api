bffb7cccaa1a6619fa1b5fc80a1008675cf09e40
Genisson Emilio <esgenisson@gmail.com>
2026-05-21 14:30:32 -0300

    fix cookies erro

--- backend/src/auth/jwt.strategy.ts
+++ backend/src/auth/jwt.strategy.ts
@@ -15,6 +15,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 
     super({
       jwtFromRequest: ExtractJwt.fromExtractors([
+        ExtractJwt.fromAuthHeaderAsBearerToken(),
         (request: Request) => {
           if (request && request.cookies) {
             return request.cookies['token'];
